25c1d5b93cfa1c835d03f189520beebafb95fa15
brucelee2000 <brucelee2000@tp.edu.tw>
2014-12-07 17:35:52 -0800

    Update with "class function" - useful in the case where no instance is needed to be generated.

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Yosemite on 12/6/14.
+//  Copyright (c) 2014 Yosemite. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image:UIImage = UIImage(named: "Ace")!
+    var isRed = true
+}
